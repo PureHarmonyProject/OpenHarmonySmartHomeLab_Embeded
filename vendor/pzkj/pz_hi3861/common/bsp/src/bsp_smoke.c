4d51d970ea5707a1ebfd7a3bd164be68281298d8
Dukiya <Dukiya@qq.com>
2025-03-29 00:09:45 +0800

    【阶段二】代码规范化已完成

--- vendor/pzkj/pz_hi3861/common/bsp/src/bsp_smoke.c
+++ vendor/pzkj/pz_hi3861/common/bsp/src/bsp_smoke.c
@@ -1,5 +1,8 @@
 #include "bsp_smoke.h"
 #include "bsp_adc.h"
+#include <unistd.h>  // for usleep
+
+#define FILTER_WINDOW_SIZE 5  // 滑动窗口大小
 
 void smoke_init(void)
 {
@@ -8,20 +11,23 @@ void smoke_init(void)
 
 int smoke_check(void)
 {
-    uint16_t smoke_val = get_adc6_value();
-    printf("smoke_val = %d\n",smoke_val);
-    if(smoke_val > 10000) {
-        return 1;
-    }else {
-        return 0;
-    }
+    uint16_t smoke_val = smoke_get_value();  // 用滤波后的值判断
+    printf("smoke_val = %d\n", smoke_val);
+    return smoke_val > 10000 ? 1 : 0;
 }
 
+// 加入滑动平均滤波
 uint16_t smoke_get_value(void)
 {
-    return get_adc6_value();
+    uint32_t sum = 0;
+    for (int i = 0; i < FILTER_WINDOW_SIZE; i++) {
+        sum += get_adc6_value();
+        usleep(3000);  // 小延时，防止ADC抖动重复
+    }
+    return sum / FILTER_WINDOW_SIZE;
 }
 
+// MQ5 燃气传感器部分同样加入滤波
 void MQ5_init(void)
 {
     adc2_init();
@@ -29,9 +35,15 @@ void MQ5_init(void)
 
 uint16_t MQ5_get_value(void)
 {
-    return get_adc2_value();
+    uint32_t sum = 0;
+    for (int i = 0; i < FILTER_WINDOW_SIZE; i++) {
+        sum += get_adc2_value();
+        usleep(3000);
+    }
+    return sum / FILTER_WINDOW_SIZE;
 }
 
+// 光照传感器保留原始（如需也加可复制模板）
 void light_init(void)
 {
     adc3_init();
